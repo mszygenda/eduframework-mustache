a94a287913947aac79389a4b94b532a5485655ba
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2012-04-09 16:21:14 +0200

    added template-rendered event support

--- ef-mustache.js
+++ ef-mustache.js
@@ -4,9 +4,14 @@ var mu = require('mu2');
 var MustacheTemplateEngine = function() {
 
   this.render = function(viewFile, viewContext, output) {
-    console.log('rendering ' + viewFile);
-    stream = mu.compileAndRender(viewFile, viewContext.viewModel)
+    var self = this;
 
+    console.log('rendering template ' + viewFile);
+    stream = mu.compileAndRender(viewFile, viewContext.viewModel)
+    stream.on('end', function () {
+      self.emit('template-rendered', viewFile, viewContext, output);
+    })
+    
     util.pump(stream, output)
   }
 }
